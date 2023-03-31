fd986907d556c07ca426c77181473120f0749f0d
simeon <simkaned@gmail.com>
2023-03-31 15:41:05 +0300

    added rotation matrix visualization

---
+++ practices/03_rb_dynamics/animate_rotation.py
@@ -0,0 +1,34 @@
+import numpy as np
+from visualize import visualize_rotation
+
+# ////////////////////////
+# generate rotation matrix
+
+def z_rotation(theta):
+    R = np.array([[np.cos(theta), -np.sin(theta), 0],
+                  [np.sin(theta), np.cos(theta), 0],
+                  [0, 0, 1]])
+    return R
+
+N = 1000
+tf = 5
+t = np.linspace(0, tf, N)
+rm = np.zeros([N, 3, 3])
+theta = np.zeros(N)
+
+for k in range(N):
+    theta[k] = 3*t[k]
+    rm[k, :, :] = z_rotation(theta[k])  # np.eye(3)
+
+
+
+
+# ////////////////////////////////////////////////
+# animate the rotation matrix and two scalar stats
+
+stats = [['time', t], 
+         ['theta', theta]]
+
+visualize_rotation(rm, 
+                   stats=stats)
+
---
+++ practices/03_rb_dynamics/visualize.py
@@ -0,0 +1,103 @@
+from matplotlib import pyplot as plt, animation
+import numpy as np
+
+
+def visualize_rotation(rotation_matrices,
+                       #  output_file=None,
+                       stats=None,
+                       verbose=True,
+                       show=True):
+
+    axes_t = np.array([rotation_matrices[:, :, 0],
+                       rotation_matrices[:, :, 1],
+                       rotation_matrices[:, :, 2]])
+
+    N = len(axes_t[0])
+    frames = N
+    interval = 1/N
+
+    # ////////////////////
+    # Animate the solution
+    # ////////////////////
+
+    # Set up figure & 3D axis for animation
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+    ax.axis('off')
+
+    # choose a different color for each trajectory
+    colors = ['r', 'g', 'b']
+
+    # set up lines and points
+    lines = sum([ax.plot([], [], [], '--', c=c, alpha=0.3)
+                for c in colors], [])
+    axes = sum([ax.plot([], [], [], '-', c=c, lw=3)
+                for c in colors], [])
+    pts = sum([ax.plot([], [], [], 'o', c=c, lw=5)
+               for c in colors], [])
+
+    corner_text = ax.text(0.02, 0.90, 1.1, '')
+
+    ax.set_xlim((-1.1, 1.1))
+    ax.set_ylim((-1.1, 1.1))
+    ax.set_zlim((-1.1, 1.1))
+
+    ax.view_init(14, 0)
+
+    def init():
+        for line, pt, axs, xi in zip(lines, pts, axes, axes_t):
+            x, y, z = xi[:0].T
+            line.set_data(x[-lag:], y[-lag:])
+            line.set_3d_properties(z[-lag:])
+            axs.set_data(np.hstack((0, x[-1:])), np.hstack((0, y[-1:])))
+            axs.set_3d_properties(np.hstack((0, z[-1:])))
+
+        return lines + pts + axes
+
+    lag = 35
+    
+    
+
+    
+    def animate(i):
+        rate = 1
+        j = (rate * i) % N
+
+        stat_text = ''
+        
+        for line, pt, axs, xi in zip(lines, pts, axes, axes_t):
+            x, y, z = xi[:j].T
+            line.set_data(x[-lag:], y[-lag:])
+            line.set_3d_properties(z[-lag:])
+            axs.set_data(np.hstack((0, x[-1:])), np.hstack((0, y[-1:])))
+            axs.set_3d_properties(np.hstack((0, z[-1:])))
+
+
+            pt.set_data(x[-1:], y[-1:])
+            pt.set_3d_properties(z[-1:])
+            
+        if stats is not None:
+            for stat in stats:
+                stat_text += rf'{stat[0]}:  {round(stat[1][j],3)}'
+                stat_text +='\n'
+            corner_text.set_text(stat_text)
+
+        # ax.view_init(30, 0.3)
+        fig.canvas.draw()
+        return lines + pts + axes
+    
+    if verbose:
+        print('Animation begin...')
+        print('Hit CTRL+W to exit')
+
+    # instantiate the animator.
+    anim = animation.FuncAnimation(fig, animate,
+                                   init_func=init,
+                                   frames=frames,
+                                   interval=interval,
+                                   blit=True)
+    if show:
+        plt.show()
+    # else:
+    #     anim.save('animation.gif', writer='imagemagick', fps=60)
+    return anim
